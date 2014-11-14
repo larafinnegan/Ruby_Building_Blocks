2700ec61abb62f1f5951dc16261ce5abbb8cda99
Lara Finnegan <lcfinnegan@gmail.com>
2014-11-14 00:11:22 -0600

    fixed a bug with the max shift

--- Caesar_Cipher.rb
+++ Caesar_Cipher.rb
@@ -1,23 +1,26 @@
 def cipher(input, shift)
     letters = input.split("")
-    alphabet = ('a'..'z').to_a + ('A'..'Z').to_a
+    l_alphabet = ('a'..'z').to_a
+    u_alphabet = ('A'..'Z').to_a
     
     letters.map! do |letter| 
-        if alphabet.include?(letter)
-            if letter == alphabet[-1]
-               letter = alphabet[shift+25]
-            elsif letter == alphabet[25]
-               letter = alphabet[shift-1]
-            else
-               new_index = alphabet.index(letter)+shift
-               letter = alphabet[new_index]
-            end
-        else
-            letter
-        end
+      if l_alphabet.include?(letter)
+        new_index = l_alphabet.index(letter) + shift
+          if new_index > 25
+            new_index = (26 - new_index).abs
+          end
+        letter = l_alphabet[new_index]
+      elsif u_alphabet.include?(letter)
+        new_index = u_alphabet.index(letter) + shift
+          if new_index > 25
+            new_index = (26 - new_index).abs
+          end
+        letter = u_alphabet[new_index]
+      else
+        letter
+      end
     end
     letters.join("")
-    
-    end
+end
         
-cipher("Zzloaa!!! hello thErE&%$", 1)
+cipher("abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ .,!", -4)
